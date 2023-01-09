cb05163eb23861b02818ac8dc518cda2fe9e67a9
Thomas Lavergne <thomas.lavergne@met.no>
2023-01-09 11:02:20 +0100

    note that the figs can be produced by a script, not necessarily a notebook

--- make_figs.py
+++ make_figs.py
@@ -26,7 +26,7 @@ fig_cnt = 0
 # Maps of buoy coverage (6 panels)
 # =======================================================
 fig_cnt += 1
-# 1. run the notebook
+# 1. run the notebook/script
 
 # 2. assemble the panels into a publication-ready file
 nh_files = ['figs/mrg_wind_pamb_ssmiamsr_{a:}_{p:}_map.png'.format(p=pstr,a='NH') for pstr in ('1991-2000','2001-2010','2011-2020')]
@@ -42,7 +42,7 @@ check_call(cmd, shell=True)
 # Example maps free-drift parameters (1 for NH, 1 for SH)
 # =======================================================
 fig_cnt += 1
-# 1. run the notebook
+# 1. run the notebook/script
 
 # 2. assemble the panels into a publication-ready file
 cmd = 'cp figs/freedriftparameter_nh_3panels.png {}/fig{:02d}.png'.format(final_dir, fig_cnt)
@@ -56,7 +56,7 @@ check_call(cmd, shell=True)
 # Example maps of t0, t1, and tdiff
 # =================================
 fig_cnt += 1
-# 1. run the notebook
+# 1. run the notebook/script
 
 # 2. assemble the panels into a publication-ready file
 nh_files = ['figs/ice_drift_{a:}_ease2-750_amsr2-gw1_201502251200-201502261200_{t:}_simple_{a:}-ease-wide.png'.format(t=tstr,a='nh') for tstr in ('t0','t1','tdiff')]
@@ -71,7 +71,7 @@ check_call(cmd, shell=True)
 # Example maps of dX and dY
 # =================================
 fig_cnt += 1
-# 1. run the notebook
+# 1. run the notebook/script
 
 # 2. assemble the panels into a publication-ready file
 nh_files = ['figs/ice_drift_{a:}_ease2-750_cdr-v1p0_24h-201001171200_{d:}_simple_{a:}-ease-eur.png'.format(d=dstr,a='nh') for dstr in ('dX','dY',)]
@@ -86,7 +86,7 @@ check_call(cmd, shell=True)
 # Validation results (hexdXdY) for ssmi-f11 and amsr2-gw1 (winter)
 # =================================
 fig_cnt += 1
-# 1. run the notebook
+# 1. run the notebook/script
 
 # 2. assemble the panels into a publication-ready file
 nh_files = ['figs/{s:}_{A:}_winter{a:}_hexdXdY.png'.format(s=sid, a='nh', A='NH') for sid in ('ssmi-f11', 'ssmis-f17', 'amsr2_tb37')]
@@ -101,7 +101,7 @@ check_call(cmd, shell=True)
 # Validation results (hexdXdY) for wind-driven motion (summer)
 # =================================
 fig_cnt += 1
-# 1. run the notebook
+# 1. run the notebook/script
 
 # 2. assemble the panels into a publication-ready file
 cmd = 'convert figs/wind_pamb_ssmiamsr_NH_summernh_hexdXdY.png figs/wind_pamb_ssmiamsr_SH_summersh_hexdXdY.png -append {}/fig{:02d}.png'.format(final_dir, fig_cnt)
@@ -110,7 +110,7 @@ check_call(cmd, shell=True)
 # Validation results (hexdXdY) for multi-source motion (year-round)
 # =================================
 fig_cnt += 1
-# 1. run the notebook
+# 1. run the notebook/script
 
 # 2. assemble the panels into a publication-ready file
 cmd = 'convert figs/mrg_wind_pamb_ssmiamsr_NH_hexdXdY.png figs/mrg_wind_pamb_ssmiamsr_SH_hexdXdY.png -append {}/fig{:02d}.png'.format(final_dir, fig_cnt)
@@ -119,7 +119,7 @@ check_call(cmd, shell=True)
 # Validation results (timeseries) for dx (Arctic) 
 # =================================
 fig_cnt += 1
-# 1. run the notebook
+# 1. run the notebook/script
 
 # 2. assemble the panels into a publication-ready file
 cmd = 'convert figs/timeseries_winter_nh_dx_cropped.png figs/timeseries_summer_nh_dx_cropped.png +append {}/fig{:02d}.png'.format(final_dir, fig_cnt)
